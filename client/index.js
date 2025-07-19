a7dd645f1eeaf7eafb884ddf87411069698622ee
Villy G <villyg@icloud.com>
2025-07-19 14:08:13 -0400

    Step 5

---
+++ client/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+async function runClient() {
+  try {
+    // Get token from Auth server
+    const tokenResponse = await axios.get("http://auth:3001/token");
+    const token = tokenResponse.data.token;
+
+    // Use token to access protected API
+    const apiResponse = await axios.get("http://api:3002/protected", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    console.log("API Response:", apiResponse.data);
+  } catch (error) {
+    console.error(
+      "Client error:",
+      error.response ? error.response.data : error.message
+    );
+  }
+}
+
+runClient();
